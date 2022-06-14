c2f71039fb349f0fc4167afc29a9d5df4c4b1530
sudais <127001sudais01@gmail.com>
2022-06-14 15:02:59 +0800

    beginning of day1

---
+++ entry/typeof.js
@@ -0,0 +1,11 @@
+let a;
+console.log(typeof a) // undefined
+
+a = "hello world"
+console.log(typeof a) // string
+
+a = 42
+console.log(typeof a) // number
+
+a = true
+console.log(typeof a) // bootlean
